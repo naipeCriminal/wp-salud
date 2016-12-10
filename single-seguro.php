325498bc0e6b28c75a5b6840e619f9976cb86876
elvis gabriel martinez lopez <elvisgabrielmartinezlopez@MacBook-Pro-de-elvis.local>
2016-12-09 18:35:08 -0600

    ajuste template single-seguro

--- single-seguro.php
+++ single-seguro.php
@@ -57,7 +57,7 @@ get_header(); ?>
   </div>
   </div>
 
-  <div class="col-md-8 col-md-offset-2">
+  <div class="col-md-12">
     <div class="col-md-12">
       <h1>Planes y Coberturas</h1>
     </div>
@@ -78,31 +78,4 @@ get_header(); ?>
  </div>
 </div>
 
-
-<?php
-			/*
-			 * Include the post format-specific template for the content. If you want to
-			 * use this in a child theme, then include a file called called content-___.php
-			 * (where ___ is the post format) and that will be used instead.
-			 */
-			
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-			// Previous/next post navigation.
-			the_post_navigation( array(
-				'next_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Next', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Next post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-				'prev_text' => '<span class="meta-nav" aria-hidden="true">' . __( 'Previous', 'twentyfifteen' ) . '</span> ' .
-					'<span class="screen-reader-text">' . __( 'Previous post:', 'twentyfifteen' ) . '</span> ' .
-					'<span class="post-title">%title</span>',
-			) );
-
-		// End the loop.
-		endwhile;
-		?>
 <?php get_footer(); ?>
