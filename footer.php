9b0307e8cf31ed16bb404a0531e034c945386b24
Emmanuel <evazquezbravo13@gmail.com>
2016-12-16 18:47:42 -0600

    Se modifcan url de footer y heder

--- footer.php
+++ footer.php
@@ -17,13 +17,13 @@
       <h4>SERVICIOS</h4>
       <ul>
         
-        <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/Comprobantes-Fiscales.pdf">Comprobantes Fiscales</a></li>
-        <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/Cancelación-de-Póliza.pdf">Cancelación de Póliza</a></li>
-        <li><a target="_BLANK" href="#">Contactos</a></li>
-        <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/Oficinas-de-Atención-GMM-y-Salud.pdf">Oficinas de Atención (GMM y Salud)</a></li>
-        <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/Solicitud-de-Alta-GMM-Familiar.pdf">Solicitud de Alta GMM Familiar</a></li>
-        <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/Solicitud-de-Cambios.pdf">Solicitud de cambios</a></li>
-        <li><a target="_BLANK" href="#">Lista de medicamentos</a></li>
+        <li><a href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/comprobantes-fiscales-2/">Comprobantes Fiscales</a></li>
+        <li><a href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/cancelacion-de-poliza-2/">Cancelación de Póliza</a></li>
+        <li><a href="#">Contactos</a></li>
+        <li><a href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/oficinas-de-atencion-gmm-y-salud-2/">Oficinas de Atención (GMM y Salud)</a></li>
+        <li><a href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/solicitud-de-alta-gmm-familiar-2/">Solicitud de Alta GMM Familiar</a></li>
+        <li><a href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/solicitud-de-cambios-2/">Solicitud de cambios</a></li>
+        <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/lista-Medicamentos.pdf">Lista de medicamentos</a></li>
       </ul>
     </div>
     <div class="col-md-4 col-xs-6 infoAyuda">
@@ -35,17 +35,15 @@
         <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/Buro_de_Entidades-SegurosSalud.pdf">Buró de Entidades Financieras SegurosSalud</a></li>
         <li><a target="_BLANK" href="#">Condiciones Generales</a></li>
         <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/Derechos-ARCO.pdf">Derechos ARCO</a></li>
-        <li><a target="_BLANK" href="#">Glosario de términos</a></li>
+        <li><a href="#">Glosario de términos</a></li>
         <li><a target="_BLANK" href="#">Legal</a></li>
-        <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/UNE.pdf">UNE</a></li>
+        <li><a href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/une-2/">UNE</a></li>
       </ul>
     </div>
     <div class="col-md-4 col-xs-6 infoAyuda">
       <h4>INFORMACIÓN CORPORATIVA</h4>
       <ul>
-        <li><a target="_BLANK" href="#">Información Financiera</a></li>
-        <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/modulos.pdf">Modulos</a></li>
-        <li><a target="_BLANK" href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/wp-content/uploads/2016/12/oficinasRegionales.pdf">Oficinas regionales</a></li>
+        <li><a href="http://ec2-52-213-166-151.eu-west-1.compute.amazonaws.com/informacion-financiera/">Información Financiera</a></li>
       </ul>
     </div>   
     </div>
