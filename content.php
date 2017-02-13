8b92e9a3644fa96f7614693a625b1519b5fb720e
elvis gabriel <egmartinez@ids.com.mx>
2017-02-13 11:45:41 -0600

    pade-index

--- content.php
+++ content.php
@@ -16,15 +16,6 @@
 		twentyfifteen_post_thumbnail();
 	?>
 
-	<header class="entry-header">
-		<?php
-			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			else :
-				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-			endif;
-		?>
-	</header><!-- .entry-header -->
 
 	<div class="entry-content">
 		<?php
