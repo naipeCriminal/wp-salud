9e4ad46317d62b6e307d0ed411ace6818cdea78a
elvis gabriel <egmartinez@ids.com.mx>
2017-02-24 14:43:38 -0600

    css y template info

--- template-info.php
+++ template-info.php
@@ -33,7 +33,7 @@ get_header(); ?>
 							?>
 
 			        	<div class="col-xs-12 col-md-4 text-left">
-			        		<p style="height:140px; text-align: justify;line-height: 30px;"><span class="circle"><?php echo $i; ?></span> <?php  the_sub_field('descripcion_img'); ?></p>
+			        		<p style="height:230px; text-align: justify;line-height: 30px;"><span class="circle"><?php echo $i; ?></span> <?php  the_sub_field('descripcion_img'); ?></p>
 			        		
 				        	<a title="<?php  the_sub_field('descripcion_img'); ?>" class="fancybox" rel="ligthbox" href="<?php the_sub_field('imagen_info'); ?>"><img class="img-responsive" style="margin: 0 auto;" src="<?php the_sub_field('imagen_info'); ?>" alt=""></a>
 				            <div class="col-md-12 text-justify">
