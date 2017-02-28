b35e4e20d18613f4fe7515845e18432e413c436c
elvis gabriel <egmartinez@ids.com.mx>
2017-02-28 11:24:56 -0600

    paginas

--- single-filtro_seguro.php
+++ single-filtro_seguro.php
@@ -5,31 +5,25 @@ Template Name: filtro seguro
 ?>
 <?php
 get_header(); ?>
-
-       <div class="container">
-		  <div class="row">
-		    <div class="col-md-12">
-		      <h2><?php the_title(); ?></h2>
-
-		    </div>
-		  </div>
-		</div>
 		<?php the_sub_field('imagen_info'); ?>
 		<?php
 			if( have_rows('banners') ):
 				while ( have_rows('banners') ) : the_row(); 
 		?>
 					<div class="slider-filtro" style="background:url('<?php the_sub_field('imagen'); ?>');background-position:center top;background-size:cover;">
-
 						<div class="container">
 							<div class="row">
-								<h2><?php the_sub_field('titulo'); ?></h2>
-								<p><?php the_sub_field('subtitulo'); ?></p>
-								<?php the_sub_field('beneficios'); ?>
-								<a href="<?php the_sub_field('link'); ?>"><button class="boton-verde">Conóce Beneficios</button></a>
+								<div class="col-md-6">
+									<div class="descripcionFiltro">
+										<h1><?php the_sub_field('titulo'); ?></h1>
+										<h2>Beneficios</h2>
+										<p><?php the_sub_field('subtitulo'); ?></p>
+										<?php the_sub_field('beneficios'); ?>
+										<a href="<?php the_sub_field('link'); ?>"><button class="boton-verde">Conóce Beneficios</button></a>									
+									</div>									
+								</div>
 							</div>							
 						</div>
-
 					</div>
 
 		<?php
